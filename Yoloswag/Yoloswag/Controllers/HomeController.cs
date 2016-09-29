ee89bc3b18c3cca90b83e93e7aa75102e9226f8e
Nathaniel Pisarski <nathanpisarski@gmail.com>
2016-09-29 19:04:52 -0400

    Convert to ASP.NET

---
+++ Yoloswag/Yoloswag/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace Yoloswag.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			var mvcName = typeof(Controller).Assembly.GetName();
+			var isMono = Type.GetType("Mono.Runtime") != null;
+
+			ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
+			ViewData["Runtime"] = isMono ? "Mono" : ".NET";
+
+			Conversation c = new Conversation();
+
+			c.AddBot();
+			c.AddBot();
+			c.AddBot();
+
+			return View(c.GenerateConversation(10));
+		}
+
+		public ActionResult Generate(int bots, int length)
+		{
+			Conversation c = new Conversation();
+
+			for (int i = 0; i < length; i++)
+				c.AddBot();
+
+			return View(c.GenerateConversation(length));
+		}
+	}
+}
---
+++ Yoloswag/Yoloswag/Models/Conversation.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+using ChatterBotAPI;
+
+namespace Yoloswag
+{
+	public class Conversation
+	{
+		/// <summary>
+		/// The participants in the conversation, which are relayed information
+		/// that happens within the conversation.
+		/// </summary>
+		/// <value>The participants</value>
+		public List<ChatterBotSession> Participants {get; set;}
+
+		private ChatterBotFactory Factory { get; set;}
+
+		public Conversation()
+		{
+			Participants = new List<ChatterBotSession>();
+			Factory = new ChatterBotFactory();
+		}
+
+		/// <summary>
+		/// This will add a new bot to the conversation
+		/// </summary>
+		public void AddBot()
+		{
+			ChatterBot newBot = Factory.Create(ChatterBotType.CLEVERBOT);
+			ChatterBotSession botSession = newBot.CreateSession();
+
+			Participants.Add(botSession);
+		}
+
+		/// <summary>
+		/// This will generate a conversation, using "hello" as a seed.
+		/// </summary>
+		/// <returns>The conversation</returns>
+		/// <param name="length">The length, in lines, of the conversation</param></param>
+		public IEnumerable<string> GenerateConversation(int length)
+		{
+			return GenerateConversation(length, "Hello");
+		}
+
+		/// <summary>
+		/// Generates a conversation with the given string as a seed.
+		/// </summary>
+		/// <returns>The conversation</returns>
+		/// <param name="length">The length of the conversation, in lines.</param>
+		/// <param name="seed">What to originally tell the first bot.</param>
+		public IEnumerable<string> GenerateConversation(int length, string seed)
+		{
+			List<string> log = new List<string>();
+
+			string last = Participants.ToArray()[0].Think(seed);
+			for (int i = 0; i < length; i++)
+			{
+				foreach (ChatterBotSession session in Participants)
+				{
+					log.Add(last);
+					last = session.Think(last);
+				}
+			}
+
+			return log;
+		}
+	}
+}
