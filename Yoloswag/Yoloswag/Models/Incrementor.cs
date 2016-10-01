9a53ae0e3c7d20144d65c0c021d1725ac82895f3
Nathaniel Pisarski <nathanpisarski@gmail.com>
2016-10-01 09:46:54 -0400

    Added moooy documentation and convenience attributes This commit added unique identifiers for each ChatMessage in the Home body, added a lot of new explanation to it, and included bootstrap and jquery

---
+++ Yoloswag/Yoloswag/Models/Incrementor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Yoloswag
+{
+	/// <summary>
+	/// The "Holy-Shit-I-Can't-Believe-I-Had-To-Make-This" class
+	/// </summary>
+	public class Incrementor
+	{
+		/// <summary>
+		/// The freaking incremented integer
+		/// </summary>
+		/// <value>One more than whatever the hell you gave the constructor</value>
+		public int Value {get; set;}
+
+		/// <summary>
+		/// Makes one more of these ungodly unnecessary class instances
+		/// </summary>
+		/// <param name="value">Whatever integer you want to commit to this incredible anti-pattern</param>
+		public Incrementor (int value)
+		{
+			Value = value + 1; // Man that was intense
+		}
+	}
+}
+
